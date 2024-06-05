cca2dd1270cf3ae80de79f8c66b02049e5bd8675
Keza <144003584+jkeza1@users.noreply.github.com>
2024-06-05 14:02:00 +0200

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,7 @@ from fabric.api import run
 
 
 
-env.hosts = ['18.209.49.232', '54.89.11.80']
+env.hosts = ['34.203.198.97', '100.27.191.214']
 def do_pack():
     """ creates gzipped archive of the directory web_static """
     dt = datetime.utcnow()
